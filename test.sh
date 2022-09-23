eef96855a0d43922a464c8dc0601e02a4e17dd50
Jenkins <jenkins@ip-172-31-25-179.ap-south-1.compute.internal>
2022-09-23 11:37:21 +0000

    updates of war

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ cd /var/lib/jenkins/gitpush
 
 git lfs track "*.war"
 
-git add *.war
-git commit -m "run"
+git add .
+git commit -m "updates of war"
 #git config --global http.postbuffer 2097152000 
 #git config http.postBuffer 2147483648
 
-git push pract1 master --force
+git push pract1 master 
 
